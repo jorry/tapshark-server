c73085ecb135f911efd132cebe5ac4d4de844c5e
jorry <jorry@jorrydeMacBook-Pro.local>
2023-07-27 11:54:46 +0800

    ab

--- config/prodConfig.js
+++ config/prodConfig.js
@@ -3,7 +3,7 @@ var prodConfig = {
     //填写数据库连接信息，可查询数据库详情页
     username:'qiuge',//mysql数据库管理员
     password:'qqqqqq1.',//密码
-    db_host:'47.92.215.156',//主机
+    db_host:'54.95.234.204',//主机
     db_port: 10220,//端口
     db_name:'qiuge'//数据库名
 }
