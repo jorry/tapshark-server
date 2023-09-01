fe85a4e3cd9a2beef079819fdb8d2d1bcff2fb26
jorry <jorry@jorrydeMacBook-Pro.local>
2023-09-01 11:41:07 +0800

    commit host

--- config/config.js
+++ config/config.js
@@ -52,8 +52,8 @@ var prodConfig = {
     //填写数据库连接信息，可查询数据库详情页
     username:'qiuge',//mysql数据库管理员
     password:'qqqqqq1.',//密码
-    db_host:'54.95.234.204',//主机
-    // db_port: 3306,//端口
+    // db_host:'54.95.234.204',//主机
+    db_port: 3306,//端口
     db_host:'localhost',//主机
     db_name:'qiuge',//数据库名
     iOSMail: {
